1dd289e1ad9393707d60dc4df6c212e655fa694e
Mehdi Guiraud <mehdi.guiraud@enercoop.org>
2018-12-18 15:01:16 +0100

    Ajout du nombre de lignes traitées

--- conf_2_keyring.py
+++ conf_2_keyring.py
@@ -6,8 +6,10 @@ import keyring
 config = configparser.ConfigParser()
 # put here de config file you want to parse
 config.read('config.file')
-
+i=0
 for section in config.sections():
 	for clé,valeur in config.items(section):
 		keyring.set_password(section,clé,valeur.strip('\"'))
+		i= i+1
+print (str(i)+" lignes traitées")
 
